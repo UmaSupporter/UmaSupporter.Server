82029b3e79dc022897e38db2d128015139949699
Lee Suho <riemannulus@hitagi.moe>
2021-04-02 13:55:58 +0900

    feat: crawler for new support card

---
+++ crawler/__init__.py
@@ -0,0 +1,88 @@
+import shutil
+from os import path
+
+import requests
+from bs4 import BeautifulSoup
+
+from database import db_session
+from models import SupportCard, CardEvent, CardEventChoice
+
+
+def download_image(url: str, filename: str):
+    r = requests.get(url, stream=True)
+    save_path = path.join('static/images', filename)
+    if r.status_code != 200:
+        raise ConnectionError(f"Cannot download image: {save_path}")
+    with open(save_path, 'wb') as f:
+        r.raw.decode_content = True
+        shutil.copyfileobj(r.raw, f)
+
+
+def get_illust_url(soup: BeautifulSoup):
+    image_soup = soup.find('img', {'alt': 'イラスト'})
+    print()
+    image_url = image_soup['data-original']
+    return image_url
+
+
+def get_support_card(uri: str, soup: BeautifulSoup):
+    wiki_id = int(uri.split('/')[-1])
+
+    h3 = soup.find('h3')
+    card_name = h3.text.replace('の性能', '')
+    illust_uri = h3.find_next_sibling('img')['data-original']
+    image_filename = illust_uri.split('/')[-1]
+    download_image(illust_uri, image_filename)
+
+    card_table: BeautifulSoup = soup.find_all('table')[1]
+    card_field = card_table.find_all('td')
+    rare_degree = card_field[0].text
+    card_type = card_field[1].text
+    second_name = card_field[2].text
+
+    support_card = SupportCard(
+        card_name=card_name,
+        card_type=card_type,
+        card_image=image_filename,
+        gamewith_wiki_id=wiki_id,
+        rare_degree=rare_degree,
+        second_name=second_name
+    )
+    db_session.add(support_card)
+    return support_card
+
+
+def get_card_event(soup: BeautifulSoup, support_card: SupportCard):
+    choice_tables = soup.find_all('div', {"class": "uma_choice_table"})
+    for choice_table in choice_tables:
+        title = choice_table.find_previous_sibling('h3').text
+        card_event = CardEvent(title=title, support_card=support_card)
+        get_card_event_choice(choice_table, card_event)
+        db_session.add(card_event)
+
+
+def get_card_event_choice(soup: BeautifulSoup, card_event: CardEvent):
+    tr_tags = soup.find_all('tr')
+    for tr in tr_tags:
+        title = tr.find('th').text
+        effect = tr.find('td').text
+        card_event_choice = CardEventChoice(title=title, effect=effect, event=card_event)
+        db_session.add(card_event_choice)
+
+
+def crawl_new_card(uri: str):
+    # https://gamewith.jp/uma-musume/article/show/266299
+    r = requests.get(uri)
+    if r.status_code != 200:
+        return False
+
+    soup = BeautifulSoup(r.text, 'lxml')
+    card = get_support_card(uri, soup)
+
+    if SupportCard.query.filter_by(second_name=card.second_name).first():
+        return False
+
+    get_card_event(soup, card)
+    db_session.commit()
+
+    return True
