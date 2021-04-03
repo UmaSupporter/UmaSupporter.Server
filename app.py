49de5d45a7cbebe96f75112c8625ea50155708c3
Lee Suho <riemannulus@hitagi.moe>
2021-04-04 05:34:38 +0900

    feat: ops

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, send_from_directory, request
 from flask_cors import CORS
 from flask_graphql import GraphQLView
 
-from crawler import crawl_new_card
+from crawler import crawl_new_card, crawl_new_umamusume
 from database import db_session
 from schema import schema
 
@@ -40,6 +40,21 @@ def new_support_card():
     if not uri:
         return 'field: new_card_uri need.', 400
     if crawl_new_card(uri):
+        return 'ok', 201
+    else:
+        return 'crawl failed', 202
+
+
+@app.route('/ops/new/uma', methods=['POST'])
+def new_umamusume():
+    params = request.get_json()
+    uri = params.get('new_card_uri')
+    input_password = params.get('root_password')
+    if not input_password == root_password:
+        return 'field: root_password is missing or do not correct', 401
+    if not uri:
+        return 'field: new_card_uri need.', 400
+    if crawl_new_umamusume(uri):
         return 'ok', 201
     else:
         return 'crawl failed', 202
