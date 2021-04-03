c274b863284ab7e9dc44c5d4b7a8f8c919c97b43
Lee Suho <riemannulus@hitagi.moe>
2021-04-04 05:35:27 +0900

    introduce: alembic

---
+++ alembic/versions/be5119c0c923_init.py
@@ -0,0 +1,24 @@
+"""init
+
+Revision ID: be5119c0c923
+Revises: 
+Create Date: 2021-04-04 05:09:07.292737
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'be5119c0c923'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
