c2d74c4319cb1a29519f181c860043518b245c9f
Lee, Suho <iam@suho.io>
2022-04-15 14:06:27 +0000

    fix: upload

--- app.py
+++ app.py
@@ -29,6 +29,15 @@ Base.metadata.create_all(engine)
 init_login(app)
 init_admin(app, db_session)
 
+UPLOAD_FOLDER = 'static/images'
+ALLOWED_EXTENSIONS = {'png'}
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/images/<path:path>')
 def send_image(path):
@@ -41,11 +50,22 @@ def get_image():
     input_password = request.headers.get('Authorization')
     if not input_password == root_password:
         return 'field: root_password is missing or do not correct', 401
-    f = request.files['file']
-    try:
-        f.save(f"static/images/{secure_filename(f.filename)}")
-    except:
-        return 'upload failed', 409
+
+    # check if the post request has the file part
+    if 'file' not in request.files:
+        flash('No file part')
+        return redirect(request.url)
+    file = request.files['file']
+    # if user does not select file, browser also
+    # submit an empty part without filename
+    if file.filename == '':
+        flash('No selected file')
+        return redirect(request.url)
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return redirect(url_for('uploaded_file',
+                                filename=filename))
 
 
 @app.route('/ops/new/card', methods=['POST'])
