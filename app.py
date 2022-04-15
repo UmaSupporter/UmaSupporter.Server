94c184e6ceeb42004f30c148a23a171be7569ad3
Lee, Suho <iam@suho.io>
2022-04-15 14:41:47 +0000

    fix: logging

--- app.py
+++ app.py
@@ -55,18 +55,18 @@ def get_image():
     # check if the post request has the file part
     if 'file' not in request.files:
         flash('No file part')
-        return redirect(request.url)
+        return 'no file part', 400
     file = request.files['file']
     # if user does not select file, browser also
     # submit an empty part without filename
     if file.filename == '':
         flash('No selected file')
-        return redirect(request.url)
+        return 'no file name', 400
     if file and allowed_file(file.filename):
         filename = secure_filename(file.filename)
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        return redirect(url_for('uploaded_file',
-                                filename=filename))
+        print(f"{app.config['UPLOAD_FOLDER']}/{filename}")
+        return 'correct', 200
 
 
 @app.route('/ops/new/card', methods=['POST'])
